86cb0541d87d298ea29fb3e0bb9ae0e00a06ab26
Ops <krzysztof.papciak@hotmail.com>
2020-02-16 15:43:32 +0100

    Add login, register services

--- GroceryExpress/src/app/routes.ts
+++ GroceryExpress/src/app/routes.ts
@@ -2,10 +2,18 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { FoodItemComponent } from './components/food-item/food-item.component';
 import { FoodListComponent } from './components/food-list/food-list.component';
+import { AuthGuard } from 'src/guards/authGuard';
 
 export const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'food', component: FoodItemComponent },
-  { path: 'food-list', component: FoodListComponent },
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'food', component: FoodItemComponent },
+      { path: 'food-list', component: FoodListComponent }
+    ]
+  },
   { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
